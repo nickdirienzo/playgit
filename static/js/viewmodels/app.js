3d5243e4098c149aafca4c83d9823595f31a0e06
Peter Zhang <ptz@squareup.com>
2013-07-12 22:52:12 -0700

    PURPLE LOGIN

--- static/js/viewmodels/app.js
+++ static/js/viewmodels/app.js
@@ -1,8 +1,38 @@
 var AppViewModel = function(json) {
 	var self = this;
 
+	this.user = ko.observable({
+		id: 1,
+		username: "Guest"
+	});
+
+	this.userLoginUsername = ko.observable("");
+	this.userLoginPassword = ko.observable("");
+
+	// data fields
 	this.playlists = ko.observableArray();
 	_.each(json.playlists, function(playlist) {
 		self.playlists.push(new PlaylistViewModel(playlist));
 	});
+
+	// display helpers
+	this.userLabel = ko.computed(function() {
+		return self.user().username;
+	});
+
+	// functions
+	this.clickUserLabel = function() {
+		$("#user .user-label").fadeOut(150, function() {
+			$("#user #user-login").fadeIn(150);	
+		});
+	};
+	this.clickRegister = function() {};
+
+	this.submitLogin = function(formElement) {
+		debugger
+		$.post('/login', {
+			username: self.userLoginUsername(),
+			password: self.userLoginPassword()
+		})
+	};
 };
