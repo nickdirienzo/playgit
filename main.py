715f754a081c7ac525ac100464bcb35d4d82043a
Nick DiRienzo <nick@dirienzo.co>
2013-07-13 07:31:57 -0700

    If a song is not found in the db, we add it with all its glorious metadata.

--- main.py
+++ main.py
@@ -196,19 +196,30 @@ def get_playlist_diff(user, playlist_id1, rev1, playlist_id2, rev2):
 @app.route('/commit/<playlist_id>', methods=['POST'])
 @require_login
 def commit_playlist_changes(user, playlist_id):
-    song_keys = set(request.json['song_keys'])
-    if not song_keys:
+    songs = request.json['songs']
+    song_keys = list()
+    for song in songs:
+        if not Song.query.filter(Song.key == song['key']).first():
+            s = Song(song['name'], song['album'], song['artist'], song['artwork_url'], song['key'])
+            db_session.add(s)
+        song_keys.append(song['key'])
+    db_session.commit()
+    if not songs:
         return jsonify(error='no keys sent')
+
     playlist = Playlist.query.filter(Playlist.id == playlist_id and Playlist.uid == session.get('user_id')).first()
     if not playlist:
         return jsonify(error='invalid playlist')
     current_songs = set(playlist.git().getTrackIds())
+    song_keys = set(song_keys)
     added = song_keys - current_songs
     removed = current_songs - song_keys
     if len(added) > 0:
         added_msg = 'Added '
         for a in added:
             ts = Song.query.filter(Song.key == a).first()
+            if not ts:
+
             added_msg += '%s by %s ' % (ts.name, ts.artist)
     else:
         added_msg = ''
